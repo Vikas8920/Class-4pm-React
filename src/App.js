9c21f5404b21f64b0692214d6a3885dfa6631d7b
Vikas8920 <vikasmishra0306@gmail.com>
2025-01-30 16:58:39 +0530

    30-01-2025

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Form from './Component/Form'
+import './style.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Form/>
+      <h1>React is {7/2} times better with JSX</h1>
+      <div className='h1'>Hello</div>
+      <div>Hello</div>
+      <input type='text' />
+
+    </>
+  )
 }
 
-export default App;
+export default App
