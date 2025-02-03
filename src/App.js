ac097ba616650f802b97eac6a13e92b17acdb570
Vikas8920 <vikasmishra0306@gmail.com>
2025-02-03 17:20:36 +0530

    03-02-2025

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React from 'react'
-import Form from './Component/Form'
-import './style.css'
+// //////////////Class component
 
-const App = () => {
-  return (
-    <>
-      <Form/>
-      <h1>React is {7/2} times better with JSX</h1>
-      <div className='h1'>Hello</div>
-      <div>Hello</div>
-      <input type='text' />
+
+import React, { Component } from 'react'
+// import Greeting from './Component/Greeting'
+// import Counter from './Component/Counter'
+// import Logger from './Component/Logger'
+import Counter2 from './Component/Counter2'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Greeting name='Vikas'/> */}
+
+        {/* <Counter/> */}
+        {/* <Logger/> */}
+        <Counter2/>
+      </div>
+    )
+  }
+}
+
+
+
+// import React from 'react'
+// import './style.css'
+// import HelloWorld from './Component/HelloWorld'
+
+// const App = () => {
+
+  // using if statement
+  // const x = 6
+  // let text = 'Good bye'
+  // if(x>5){
+  //   text = 'Hello React'
+  // }
+
+  // using ternary Operator
+  // return (
+  //   <>
+    {/* <h1>{(x)<10?'Hello React':'Goodbye React'}</h1> */}
+    {/* <HelloWorld name={'Vikas'} para={'Welcome'}/>
+    <HelloWorld name={'Rishabh'} para={'Bye'}/> */}
+{/* 
 
     </>
   )
 }
 
-export default App
+export default App */}
+
+
+
+
+
--- src/Component/Button.js
+++ src/Component/Button.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <button>Submit</button>
+   <>
+   </>
   )
 }
 
