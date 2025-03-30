b6b0ec3c001d134d20905b09e8693292a9fa0de2
Vikas8920 <vikasmishra0306@gmail.com>
2025-03-30 18:07:01 +0530

    30-03-2025 parrt- 2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Counter from './Component/Counter'
-import AuthProvider from './Component/AuthProvider'
-import Login from './Component/Login'
-import UserProfile from './Component/UserProfile'
-import ThemeProvider from './Component/ThemeProvider'
-import ThemeToggle from './Component/ThemeToggle'
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { CartProvider } from './Context/CartContext'
+import Navbar from './Component/Navbar'
+import ProductList from './Component/ProductList'
+import Cart from './Component/Cart'
 
 const App = () => {
-  const [inputValue, setInputValue] = useState('')
-  // const count = useRef(0)
-  const previousInputValue = useRef('')
-
-  useEffect(()=>{
-    previousInputValue.current = inputValue
-  }, [inputValue])
   return (
     <>
-      <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
-      <h1>Current value : {inputValue}</h1>
-      <h1>Previous value: {previousInputValue.current}</h1>
-
-      <hr/>
-      <Counter/>
-
-      <hr/>
-
-      <AuthProvider>
-        <h1>Simple Authentication System</h1>
-        <Login/>
-        <UserProfile/>
-      </AuthProvider>
-
-      <hr/>
-
-      <ThemeProvider>
-        <h1>Toggle Theme</h1>
-        <ThemeToggle/>
-      </ThemeProvider>
+      <Router>
+        <CartProvider>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<ProductList/>}/>
+                <Route path='/cart' element={<Cart/>}/>
+            </Routes>
+        </CartProvider>
+      </Router>
     </>
   )
 }
